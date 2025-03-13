b445a805c51ef0870324cac70407eb69c932f169
Shwan <69142403+shwan26@users.noreply.github.com>
2025-03-13 15:00:14 +0700

    Update firebase.js

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAt98OZ7AANAkmO4REus-hMiNbteUqPGck",
-  authDomain: "wordle-game-a1a95.firebaseapp.com",
-  projectId: "wordle-game-a1a95",
-  storageBucket: "wordle-game-a1a95.firebasestorage.app",
-  messagingSenderId: "618792504122",
-  appId: "1:618792504122:web:189ddd563bbec4e98e1e03",
-  measurementId: "G-XGM8CDRBZ4"
+  
 };
 
 // Initialize Firebase
